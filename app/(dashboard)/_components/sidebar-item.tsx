7d91f4eb9553ec7c4319557d66c7b4a0cb63ff28
Melvin <melvinundercover@icloud.com>
2024-01-08 17:21:04 +0100

    Update on private routes

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 interface SidebarItemProps{
     icon:LucideIcon;
@@ -10,6 +11,15 @@ interface SidebarItemProps{
 
 }
 const SidebarItem = ({icon: Icon,label,href}: SidebarItemProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isActive = (pathname === '/' && href === '/' ) || pathname === href || pathname?.startsWith(`${href}/`);
+
+    const onClick = ()=>{
+        router.push(href);
+        // this is where you are 
+    }
     return ( <div>
         Sidebar Item
     </div> );
