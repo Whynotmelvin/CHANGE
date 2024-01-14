4750cc9ec8684873c267bb6c12b1cee94b2a28c5
Melvin <melvinundercover@icloud.com>
2024-01-14 12:51:51 +0100

    Course Creation

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,27 +1,76 @@
-"use client"
-import * as z  from 'zod';
-import axios from 'axios' ;
-import { useRouter } from 'next/router';
-import {useForm} from 'react-hook-form';
-import {zodResolver} from '@hookform/resolvers/zod';
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
-    Form,
-    FormControl,
-    FormLabel,
-    FormDescription,
-    FormMessage
+  Form,
+  FormControl,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+  FormField,
+  FormItem,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+});
 
-const CreatePage = ()=>{
-    return(
-        <div>
-            Create Page
-        </div>
-    )
-}
+const CreatePage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6 ">
+      <div>
+        <h1 className="text-2xl">Name your course</h1>
+        <p>What would you like to name your course ?</p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Course Title
+                  </FormLabel>
+                  <FormControl>
+                    <Input 
+                    disabled={isSubmitting}
+                    placeholder="e.g 'Advanced Web-development'"
+                    {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course ?
+                  </FormDescription>
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
 export default CreatePage;
